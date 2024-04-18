a330909bfb7f32e1ef1997aacebd94e6a34eda0c
Safarov Renat <SafarovRenat1995@gmail.com>
2024-04-18 07:51:30 +0300

    Project initialization

---
+++ src/main/java/com/example/bobomap/BobomapApplication.java
@@ -0,0 +1,13 @@
+package com.example.bobomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobomapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobomapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bobomap/BobomapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bobomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobomapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
